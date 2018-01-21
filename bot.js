54f62a17f2d90468c6544de9dfa0a82010bfc20c
heinzelmaennchen <bengau@aon.at>
2018-01-20 19:16:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,26 @@ client.on('message', message => {
     	message.reply('pong');
   	}
     if (message.content === 'seas') {
-    	message.reply('seas!');
+    	message.reply('seas!11');
+  	}
+  	if (message.content === '$BTC') {
+       let price = getCoinPrice('BTC');
+       message.reply('BTC price: ' + price + 'EUR');
   	}
 });
+
+function getCoinPrice(coinTicker) {
+  let url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+ coinTicker +'&tsyms=EUR';
+  let result = UrlFetchApp.fetch(url);
+  let data = JSON.parse(result.getContentText());
+  let coinData = data.RAW[coinTickers[ticker]].EUR;
+  let price = coinData.PRICE;
+  let dailyLow = coinData.LOW24HOUR;
+  let dailyHigh = coinData.HIGH24HOUR;
+  let pctChange = coinData.CHANGEPCT24HOUR;
+
+  return price;
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
