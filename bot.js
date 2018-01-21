bdd74a40acc013933db6d8007c7c06ef2b1d18d6
heinzelmaennchen <bengau@aon.at>
2018-01-20 19:27:06 -0800

    Testing

--- bot.js
+++ bot.js
@@ -14,25 +14,37 @@ client.on('message', message => {
   	}
   	if (message.content === '$BTC') {
        console.log('received BTC command');
-       let price = getCoinPrice('BTC');
-       message.reply('BTC price: ' + price + 'EUR');
+       //let price = getCoinPrice('BTC');
+       getCoinPrice('BTC');
+       message.reply('request received, output in console');
   	}
 });
+
+function reqListener () {
+  console.log(this.responseText);
+}
+
 
 function getCoinPrice(coinTicker) {
   let url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+ coinTicker +'&tsyms=EUR';
   console.log(url);
-  let result = UrlFetchApp.fetch(url);
-  let data = JSON.parse(result.getContentText());
-  console.log(data);
-  let coinData = data.RAW[coinTickers[ticker]].EUR;
-  console.log(coinData);
-  let price = coinData.PRICE;
-  let dailyLow = coinData.LOW24HOUR;
-  let dailyHigh = coinData.HIGH24HOUR;
-  let pctChange = coinData.CHANGEPCT24HOUR;
+  
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", url);
+  oReq.send();
 
-  return price;
+  //let result = UrlFetchApp.fetch(url);
+  //let data = JSON.parse(result.getContentText());
+  //console.log(data);
+  //let coinData = data.RAW[coinTickers[ticker]].EUR;
+  //console.log(coinData);
+  //let price = coinData.PRICE;
+  //let dailyLow = coinData.LOW24HOUR;
+  //let dailyHigh = coinData.HIGH24HOUR;
+  //let pctChange = coinData.CHANGEPCT24HOUR;
+
+  //return price;
 }
 
 // THIS  MUST  BE  THIS  WAY
