dac730b2b735dde5f93176644133678d317a31a1
heinzelmaennchen <bengau@aon.at>
2018-01-22 07:45:55 -0800

    Output one line (for real)

--- bot.py
+++ bot.py
@@ -35,9 +35,8 @@ def getCurrentValues(coin):
 
 def buildResponse(coinStats, coin):
     """Builds response string which will be printed to the channel."""
-    r = '**' + coin + '**\n'
-    r += ('Current price: ' + str(coinStats['PRICE']) + ' EUR (' + 
-          str(round(coinStats['CHANGEPCT24HOUR'],2)) + '%)\n')
+    r = ('**' + coin + ':** Current price: ' + str(coinStats['PRICE']) + ' EUR (' + 
+         str(round(coinStats['CHANGEPCT24HOUR'],2)) + '%)\n')
     return r
 
 client.run(os.environ['BOT_TOKEN'])
