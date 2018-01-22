af8b5700b35a810cab1054812964a5d936f6e787
heinzelmaennchen <bengau@aon.at>
2018-01-22 08:41:55 -0800

    Minor fixes
    Change input to case insensitive and expand coin ticker size to 4

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def on_message(message):
     if message.content.startswith('!ping'):
         await client.send_message(message.channel, 'Pong!')
     elif message.content.startswith('$'):
-        coin = message.content[1:4]
+        coin = string.upper(message.content[1:5])
         coinStats = getCurrentValues(coin)
         response = buildResponse(coinStats, coin)
         await client.send_message(message.channel, response)
