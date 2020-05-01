df7193cc9a07a422eaf2f57fce7c1d63b9df117a
heinzelmaennchen <bengau.at@gmail.com>
2020-05-01 14:59:53 +0800

    change quote number to 1

--- quote.py
+++ quote.py
@@ -9,7 +9,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def randomQuote():
   cur = conn.cursor()
-  sql = 'SELECT * FROM wlc_quotes ORDER BY RANDOM() LIMIT 2'
+  sql = 'SELECT * FROM wlc_quotes ORDER BY RANDOM() LIMIT 1'
   cur.execute(sql)
 
   row = cur.fetchone()
