761d49d1112af2f16d7bcefe57ec2ab0b55a3fd1
backslash101 <st.scheider@gmail.com>
2020-05-31 18:33:01 +0200

    fix for hltb scraper for some results

--- cogs/hltb.py
+++ cogs/hltb.py
@@ -21,15 +21,36 @@ class hltb(commands.Cog):
         embedHltb = discord.Embed(title=title,
                                   url=hltbResult[title]['url'],
                                   colour=discord.Colour.from_rgb(255, 128, 0))
-        embedHltb.add_field(name='Main Story',
-                            value=f"{hltbResult[title]['Main Story']}",
-                            inline=True)
-        embedHltb.add_field(name='Main + Extra',
-                            value=f"{hltbResult[title]['Main + Extra']}",
-                            inline=True)
-        embedHltb.add_field(name='Completionist',
-                            value=f"{hltbResult[title]['Completionist']}",
-                            inline=True)
+        try:
+            embedHltb.add_field(name='Main Story',
+                                value=f"{hltbResult[title]['Main Story']}",
+                                inline=True)
+            embedHltb.add_field(name='Main + Extra',
+                                value=f"{hltbResult[title]['Main + Extra']}",
+                                inline=True)
+            embedHltb.add_field(name='Completionist',
+                                value=f"{hltbResult[title]['Completionist']}",
+                                inline=True)
+        except:
+            pass
+        try:
+            embedHltb.add_field(name='Solo',
+                                value=f"{hltbResult[title]['Solo']}",
+                                inline=True)
+        except:
+            pass
+        try:
+            embedHltb.add_field(name='Co-Op',
+                                value=f"{hltbResult[title]['Co-Op']}",
+                                inline=True)
+        except:
+            pass
+        try:
+            embedHltb.add_field(name='Vs.',
+                                value=f"{hltbResult[title]['Vs.']}",
+                                inline=True)
+        except:
+            pass
 
         await ctx.send(embed=embedHltb)
 
@@ -39,14 +60,14 @@ class hltb(commands.Cog):
 def HLTB(title):
     try:
         times, title, url = FindGame(title)
-        return {
+        hltbResult = {
             title: {
                 'url': url,
-                'Main Story': times[0][1],
-                'Main + Extra': times[1][1],
-                'Completionist': times[2][1]
             }
         }
+        for i in range(0, len(times)):
+            hltbResult[title].update({times[i][0]: times[i][1]})
+        return hltbResult
     except:
         raise
 
@@ -64,11 +85,25 @@ def FindGame(title):
     #title,url = tmp['title'], HLTB_PRE + tmp['href'] ORIGINAL
     title, url = tmp.text, HLTB_PRE + tmp['href']
     scrape = page.findAll("div", class_="search_list_tidbit")
-    result = [(scrape[0].text.split(' ')[1],
-               "{} Hrs".format(scrape[1].text.split(' ')[0])),
-              (''.join(scrape[2].text.split(' ')[1:]),
-               "{} Hrs".format(scrape[3].text.split(' ')[0])),
-              (scrape[4].text, "{} Hrs".format(scrape[5].text.split(' ')[0]))]
+    result = []
+    try:
+        result = [
+            (scrape[0].text, "{} Hrs".format(scrape[1].text.split(' ')[0])),
+            (scrape[2].text, "{} Hrs".format(scrape[3].text.split(' ')[0])),
+            (scrape[4].text, "{} Hrs".format(scrape[5].text.split(' ')[0]))
+        ]
+    except IndexError:
+        pass
+    scrape = [
+        page.findAll("div", class_="search_list_tidbit_short"),
+        page.findAll("div", class_="search_list_tidbit_long")
+    ]
+    try:
+        for i in range(0, len(scrape[0])):
+            result.append((scrape[0][i].text,
+                           "{} Hrs".format(scrape[1][i].text.split(' ')[0])))
+    except IndexError:
+        pass
     return result, title, url
 
 
