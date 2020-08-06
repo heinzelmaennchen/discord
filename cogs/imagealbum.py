79e502ff61c4401dfdf7e530004877c15eb5a33e
backslash101 <st.scheider@gmail.com>
2020-08-06 09:30:36 +0200

    fix for multiple attachments in one msg

--- cogs/imagealbum.py
+++ cogs/imagealbum.py
@@ -16,46 +16,47 @@ class imagealbum(commands.Cog):
             return
 
         if message.attachments:
-            att = message.attachments
-
+            atts = message.attachments
             pic_ext = ['.jpg', '.png', '.jpeg', '.gif']
-            for ext in pic_ext:
-                if att[0].filename.endswith(ext):
-                    try:
-                        #AUTH TOKEN
-                        service = utils.googlephotos.init_service()
-                        token = pickle.load(
-                            open('storage/auth/token_photoslibrary_v1.pickle',
-                                 'rb'))
-                        #print(f'Token: {token}\n')
-                        #ALBUM TITLE
-                        album_title = f'{message.guild.name} #{message.channel.name}'
-                        #print(f'Album Title: {album_title}\n')
-                        #ALBUMID
-                        albumId = utils.googlephotos.get_albumId(
-                            album_title, service)
-                        if albumId == None:
-                            albumId = utils.googlephotos.create_album(
+            try:
+                for att in atts:
+                    for ext in pic_ext:
+                        if att.filename.endswith(ext):
+                            #AUTH TOKEN
+                            service = utils.googlephotos.init_service()
+                            token = pickle.load(
+                                open(
+                                    'storage/auth/token_photoslibrary_v1.pickle',
+                                    'rb'))
+                            #print(f'Token: {token}\n')
+                            #ALBUM TITLE
+                            album_title = f'{message.guild.name} #{message.channel.name}'
+                            #print(f'Album Title: {album_title}\n')
+                            #ALBUMID
+                            albumId = utils.googlephotos.get_albumId(
                                 album_title, service)
-                        #print(f'AlbumId: {albumId}\n')
-                        #FILENAME
-                        time = getMessageTime(message)
-                        filenametime = f'{time.year}{time.month}{time.day}{time.hour}{time.minute}{time.second}'
-                        filename = os.path.splitext(att[0].filename)[0]
-                        filename = filenametime + '_' + filename + os.path.splitext(
-                            att[0].filename)[1]
-                        #print(f'File Name: {filename}\n')
+                            if albumId == None:
+                                albumId = utils.googlephotos.create_album(
+                                    album_title, service)
+                            #print(f'AlbumId: {albumId}\n')
+                            #FILENAME
+                            time = getMessageTime(message)
+                            filenametime = f'{time.year}{time.month}{time.day}{time.hour}{time.minute}{time.second}'
+                            filename = os.path.splitext(att.filename)[0]
+                            filename = filenametime + '_' + filename + os.path.splitext(
+                                att.filename)[1]
+                            #print(f'File Name: {filename}\n')
 
-                        #IMAGE BYTE STREAM
-                        image_bytes = await att[0].read()
+                            #IMAGE BYTE STREAM
+                            image_bytes = await att.read()
 
-                        mediaItem = utils.googlephotos.upload_image_to_album(
-                            albumId, image_bytes, filename, token, service)
-                        #print(f'Media Item: {mediaItem}')
+                            mediaItem = utils.googlephotos.upload_image_to_album(
+                                albumId, image_bytes, filename, token, service)
+                            #print(f'Media Item: {mediaItem}')
 
-                        await message.add_reaction('⬆️')
-                    except:
-                        await message.add_reaction('❌')
+                            await message.add_reaction('⬆️')
+            except:
+                await message.add_reaction('❌')
 
 
 def setup(client):
