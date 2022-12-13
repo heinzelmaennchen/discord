ba91f912d701e0ef64762d670082fd83ae9e208d
backslash101 <35721505+backslash101@users.noreply.github.com>
2022-12-13 20:08:10 +0100

    add zero width space at code block start

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -355,7 +355,7 @@ class crypto(commands.Cog):
         else:
             currency_symbol = 'N/A'
 
-        r = '```\n'
+        r = '```\n​'
         for x in range(len(symbols)):
             r += ((symbols[x]).rjust(coinwidth) + ': ' +
                   (values[x]).rjust(valuewidth) + ' ' + currency_symbol +
