84114339b855f4f9195ebf5ac9d8dfba2d8dbf18
backslash101 <35721505+backslash101@users.noreply.github.com>
2023-04-01 11:27:18 +0200

    added open ai image generation

--- cogs/chatgpt.py
+++ cogs/chatgpt.py
@@ -34,6 +34,29 @@ class chatgpt(commands.Cog):
         except Exception as e:
             # Send an error message to the Discord channel if there was an issue with the API request
             await ctx.send(f"Sorry, there was an error generating the text. Error message: {str(e)}")
+    
+    @commands.command(name="img")
+    @commands.guild_only()
+    async def generate_image(self, ctx, *, prompt):
+        try:
+            # Generate image using the OpenAI API
+            # generate 1 image, 1024x1024px
+            response = openai.Image.create(
+                prompt=prompt,
+                n=1,
+                size="1024x1024"
+            )
+            image_url = response['data'][0]['url']
+            # Send the generated image as an embed in the Discord channel
+            embed = discord.Embed(colour=discord.Colour.from_rgb(47, 49, 54))
+            embed.set_author(
+                name="WLC GPT",
+                icon_url="https://townsquare.media/site/295/files/2019/10/Terminator-Orion.jpg")
+            embed.set_image(url=image_url)
+            await ctx.send(embed=embed)
+        except Exception as e:
+            # Send an error message to the Discord channel if there was an issue with the API request
+            await ctx.send(f"Sorry, there was an error generating the image. Error message: {str(e)}")
 
 
 # Initialize the Discord bot and add the GPT cog
