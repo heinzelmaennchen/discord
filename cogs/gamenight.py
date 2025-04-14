ce9fb4a0b45f0e46ea976bdc930ed7250fab0513
backslash101 <35721505+backslash101@users.noreply.github.com>
2025-04-14 09:40:36 +0200

    added auto_archive_duration to gn create_thread, changed gn thread time

--- cogs/gamenight.py
+++ cogs/gamenight.py
@@ -12,7 +12,7 @@ from utils.db import init_db
 from utils.misc import getDatetimeNow
 
 my_timezone = zoneinfo.ZoneInfo("Europe/Vienna")
-gn_pollthread_time = time(20, 0, 0, tzinfo=my_timezone)
+gn_pollthread_time = time(21, 33, 37, tzinfo=my_timezone)
 
 class gamenight(commands.Cog):
     def __init__(self, client):
@@ -32,24 +32,27 @@ class gamenight(commands.Cog):
     async def gn_pollthread(self) -> None:
         now = getDatetimeNow()
         weekday = now.isoweekday()
-        if weekday % 5 == 0:    # True if weekday is %1: Monday, %2: Tuesday, %3: Wednesday, %4: Thursday, %5: Friday, %6: Saturday, %7: Sunday
-            guild = self.client.get_guild(self.guild_id)
-            channel = guild.get_channel(self.channel_id)
-            emojis = guild.emojis
+        if weekday % 4 != 0:
+            # If weekday is not %1: Monday, %2: Tuesday, %3: Wednesday, %4: Thursday, %5: Friday, %6: Saturday, %7: Sunday then return/skip
+            return
+        
+        guild = self.client.get_guild(self.guild_id)
+        channel = guild.get_channel(self.channel_id)
+        emojis = guild.emojis
 
-            thread = await channel.create_thread(name = f'Zocktag {(now + timedelta(days=3)).strftime("%d.%m.")}-{(now + timedelta(days=7)).strftime("%d.%m.")}')
-            poll = discord.Poll(question="Zockermittwoch wen wen wen?", duration=timedelta(days=3), multiple=True)
-            poll.add_answer(text=f'Montag {(now + timedelta(days=3)).strftime("%d.%m.")}', emoji=random.choice(emojis))
-            poll.add_answer(text=f'Dienstag {(now + timedelta(days=4)).strftime("%d.%m.")}', emoji=random.choice(emojis))
-            poll.add_answer(text=f'Mittwoch {(now + timedelta(days=5)).strftime("%d.%m.")}', emoji=random.choice(emojis))
-            poll.add_answer(text=f'Donnerstag {(now + timedelta(days=6)).strftime("%d.%m.")}', emoji=random.choice(emojis))
-            poll.add_answer(text=f'Freitag {(now + timedelta(days=7)).strftime("%d.%m.")}', emoji=random.choice(emojis))
-            await thread.send(poll=poll)
-            mention_members = self.getMembers(guild.get_channel(self.users_channel_id))
-            r = f''
-            for member in mention_members:
-                r += f'{member.mention} '
-            await thread.send(r)
+        thread = await channel.create_thread(name=f'Zocktag {(now + timedelta(days=3)).strftime("%d.%m.")}-{(now + timedelta(days=7)).strftime("%d.%m.")}', auto_archive_duration=10080)
+        poll = discord.Poll(question="Zockermittwoch wen wen wen?", duration=timedelta(days=3), multiple=True)
+        poll.add_answer(text=f'Montag {(now + timedelta(days=3)).strftime("%d.%m.")}', emoji=random.choice(emojis))
+        poll.add_answer(text=f'Dienstag {(now + timedelta(days=4)).strftime("%d.%m.")}', emoji=random.choice(emojis))
+        poll.add_answer(text=f'Mittwoch {(now + timedelta(days=5)).strftime("%d.%m.")}', emoji=random.choice(emojis))
+        poll.add_answer(text=f'Donnerstag {(now + timedelta(days=6)).strftime("%d.%m.")}', emoji=random.choice(emojis))
+        poll.add_answer(text=f'Freitag {(now + timedelta(days=7)).strftime("%d.%m.")}', emoji=random.choice(emojis))
+        await thread.send(poll=poll)
+        mention_members = self.getMembers(guild.get_channel(self.users_channel_id))
+        r = f''
+        for member in mention_members:
+            r += f'{member.mention} '
+        await thread.send(r)
     
     def getMembers(self, channel) -> list:
         # getChannelMembers from "main channel" to mention and add them into the thread
@@ -103,7 +106,6 @@ class gamenight(commands.Cog):
     def getDayString(self, day):
         days = ['Mo','Di','Mi','Do','Fr','Sa','So']
         return days[day]
-
 
 
 async def setup(client):
